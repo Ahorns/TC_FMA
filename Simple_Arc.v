ab5595f6c351ae4cab84233caa63d1dd039c461f
Sherlock Song <68814970+Ahorns@users.noreply.github.com>
2024-06-14 22:28:31 +0800

    Add files via upload

--- Simple_Arc.v
+++ Simple_Arc.v
@@ -4,43 +4,51 @@
 // Notes 1.0: The output of first stage is the sum and carry.
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module Simple_Arc(
-    input [255:0] a,
-    input [255:0] b,
-    output [511:0] result
+module Simple_Arc (
+    input  [10:0] U_IN,
+    input  [10:0] V_IN,
+    output [21:0] Result
 );
 
-    // 将输入a和b分成3个部分
-    wire [63:0] a0, a1, a2;
-    wire [63:0] b0, b1, b2;
-    assign {a2, a1, a0} = a;
-    assign {b2, b1, b0} = b;
+    // Internal variables
+    wire [3:0] U0, U1, U2, V0, V1, V2;
+    wire [5:0] A, B, C, D, E//WB0, WB1, WB2, WBm1, WBinf;
+    wire [21:0] t1, t2, tm1, dU1, dV1;
+    integer k, r, ss;
 
-    // 计算中间结果
-    wire [127:0] v0, v1, v2, vm1, vinf;
-    wire [127:0] da1, db1, t1, t2, tm1;
+    // Extract slices
+    assign U2 = U_IN[10:8];
+    assign U1 = U_IN[7:4];
+    assign U0 = U_IN[3:0];
+    assign V2 = V_IN[10:8];
+    assign V1 = V_IN[7:4];
+    assign V0 = V_IN[3:0];
 
-    assign v0 = a0 * b0;
-    assign da1 = a2 + a0;
-    assign db1 = b2 + b0;
-    assign vm1 = (da1 - a1) * (db1 - b1);
-    assign da1 = da1 + a1;
-    assign db1 = db1 + b1;
-    assign v1 = da1 * db1;
-    assign v2 = (da1 + a2) << 1 - a0 * (db1 + b2 << 1 - b0);
-    assign vinf = a2 * b2;
+    // Perform Toom-Cook 3-way multiplication
+    assign dU1 = U2 + U0;
+    assign dV1 = V2 + V0;
+    assign A   = U0 * V0;
+    assign D = (dU1 - U1) * (dV1 - V1);
+    assign dU1 = dU1 + U1;
+    assign dV1 = dV1 + V1 ;
+    assign B = dU1 * dV1;
+    assign C = (((dU1 + U2) << 1) - U0) * ((dV1 + V2) << 1 - V0);
+    assign E = U2 * V2;
 
-    // 执行除法操作
-    assign t2 = (v2 - vm1) / 3;
-    assign tm1 = (v1 - vm1) >> 1;
-    assign t1 = v1 - v0;
-    assign t2 = (t2 - t1) >> 1;
-    assign t1 = t1 - tm1 - vinf;
-    assign t2 = t2 - (vinf << 1);
+    // Exact division by 2 and 3
+    assign t2 = (C - D) / 3;
+    assign tm1 = (B - D) >> 1;
+    assign t1 = B - A;
+    assign t2 = (t2 - t1) >> 1 - (E << 1);
+    /*assign t1 = t1 - tm1 - E;
+    assign tm1 = tm1 - t2;*/
+    assign t1 = t1 - tm1;
+
+    assign t2 = E * 16 + t2;
+    assign tm1 = t1 * 16 + tm1;
     assign tm1 = tm1 - t2;
 
-    // 组合最终结果
-    assign result = {vinf << (3*64), t2 << (2*64), t1 << 64, tm1, v0};
+    assign Result = t2 * (16^3) + tm1 * 16 + t1;
+
 
 endmodule
